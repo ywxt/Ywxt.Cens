1cc95a7bee92aec62e28085611890fba4d242362
ywxt <ywxtcwh@qq.com>
2020-09-11 23:07:14 +0800

    UnknownInstructionException 指令显示为两位16进制数

--- src/Ywxt.Cens.Core/Exceptions/UnknownInstructionException.cs
+++ src/Ywxt.Cens.Core/Exceptions/UnknownInstructionException.cs
@@ -4,7 +4,7 @@ namespace Ywxt.Cens.Core.Exceptions
 {
     public class UnknownInstructionException : Exception
     {
-        public UnknownInstructionException(byte instruction) : base($"未知的指令: {instruction:X}")
+        public UnknownInstructionException(byte instruction) : base($"未知的指令: {(int) instruction:X2}")
         {
         }
     }
